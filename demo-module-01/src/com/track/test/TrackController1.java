5ccbdde3ab00dce3a501c03a56e4fd63503a976c
ganzk <gan141425@163.com>
2020-06-03 18:24:55 +0800

    第四次提交

--- demo-module-01/src/com/track/test/TrackController1.java
+++ demo-module-01/src/com/track/test/TrackController1.java
@@ -3,7 +3,7 @@ package com.track.test;
 public class TrackController1 {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("hello,java");
     }
 
 }
